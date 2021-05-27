e1510b3274c8e4951d9d57e9087771481f8a21fe
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-27 19:46:38 +0200

    [add] field: inputLength, specialChars, frazeLength, stringList

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -1,10 +1,28 @@
 package karnecki.blazej;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Task {
 
-    private void logicOccurrences(String input){
-        
+    private void logicOccurrences(String input) {
+
+        Set<Character> charactersSetLOGIC = new HashSet<>();
+
+        int logicInWordCounter = 0;
+
+        int inputLength = input.length();
+        int specialChars = 0;
+        int frazeLength = inputLength - specialChars;
+
+        List<String> stringList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+
+
+
+
+
     }
+
     public static void main(String[] args) {
 
     }
