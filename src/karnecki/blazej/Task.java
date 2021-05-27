08a1409003f258254e42df3cbd79266e268c7426
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-27 20:53:23 +0200

    [add] set: charsInWord

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -7,24 +7,34 @@ public class Task {
 
     private static void logicOccurrences(String input) {
 
-        Set<Character> charactersSetLOGIC = new HashSet<>();
+        Set<Character> charactersSetLOGIC = Set.of('L', 'O', 'G', 'I', 'C');
 
         int logicInWordCounter = 0;
-        String inputWithNoSpecialCharsWithSpaces = input.replaceAll("[^a-zA-Z0-9]", " ");
+        String inputWithNoSpecialCharsWithSpaces = input.toUpperCase().replaceAll("[^a-zA-Z0-9]", " ");
 
         int inputLength = input.length();
-        System.out.println(input.length());
+//        System.out.println(input.length());
 
         String inputWithNoSpecialCharsAndNoSpaces = input.replaceAll("[^a-zA-Z0-9]", "");
-
         int frazeLength = inputWithNoSpecialCharsAndNoSpaces.length();
         System.out.println(frazeLength);
 
-        List<String> stringList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
+        // Check list of split input
+//      Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList()).forEach(s -> System.out.println(s));
+        List<String> stringList = Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList());
 
 
-
-
+        for (String s: stringList) {
+            Map<Set<String>, Integer> setIntegerMap = new HashMap<>();
+            Set<String> charsInWord = new HashSet<>();
+            for (Character c : charactersSetLOGIC) {
+                if(s.contains(c + "")){
+                    charsInWord.add(String.valueOf(c));
+                }
+            }
+            System.out.println(charsInWord);
+//            System.out.println(setIntegerMap.put(charsInWord, s.length()));
+        }
 
     }
 
