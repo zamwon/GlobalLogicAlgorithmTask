ef2fd9d7247d138ec3a8e3bb997f7e342d3ea4e6
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-27 22:10:03 +0200

    [add] logicOccur in Key

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -9,38 +9,39 @@ public class Task {
 
         Set<Character> charactersSetLOGIC = Set.of('L', 'O', 'G', 'I', 'C');
 
-        int logicInWordCounter = 0;
         String inputWithNoSpecialCharsWithSpaces = input.toUpperCase().replaceAll("[^a-zA-Z0-9]", " ");
 
         int inputLength = input.length();
-//        System.out.println(input.length());
 
         String inputWithNoSpecialCharsAndNoSpaces = input.replaceAll("[^a-zA-Z0-9]", "");
         int frazeLength = inputWithNoSpecialCharsAndNoSpaces.length();
-        System.out.println(frazeLength);
 
         // Check list of split input
 //      Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList()).forEach(s -> System.out.println(s));
         List<String> stringList = Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList());
 
+        Map<Map<Set<String>, Integer>, Integer> resultDict = new HashMap<>();
 
-        for (String s: stringList) {
-            Map<Set<String>, Integer> setIntegerMap = new HashMap<>();
-            Set<String> charsInWord = new HashSet<>();
-            for (Character c : charactersSetLOGIC) {
-                if(s.contains(c + "")){
-                    charsInWord.add(String.valueOf(c));
+            for (String s : stringList) {
+                Map<Set<String>, Integer> setIntegerMap = new HashMap<>();
+                Set<String> charsInWord = new HashSet<>();
+                int logicInKeyCounter = 0;
+                for (Character c : charactersSetLOGIC) {
+                    if (s.contains(String.valueOf(c))) {
+                        charsInWord.add(String.valueOf(c));
+                        logicInKeyCounter++;
+                    }
                 }
+                setIntegerMap.put(charsInWord, s.length());
+                System.out.println(charsInWord + ", " + setIntegerMap.get(charsInWord) + " logicC= " + logicInKeyCounter);
             }
-            System.out.println(charsInWord);
-//            System.out.println(setIntegerMap.put(charsInWord, s.length()));
-        }
+
 
     }
 
     public static void main(String[] args) {
 
-        logicOccurrences("I love to work in global logic!");
+        logicOccurrences("I love to work in global logic plate level!");
     }
 
 }
