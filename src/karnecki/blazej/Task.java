b59fb62dacce27af8a07dd878f61e81732b7f3c5
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-27 19:56:31 +0200

    [delete] field: inputLength, specialChars [rename] field: frazeLength

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -5,15 +5,20 @@ import java.util.stream.Collectors;
 
 public class Task {
 
-    private void logicOccurrences(String input) {
+    private static void logicOccurrences(String input) {
 
         Set<Character> charactersSetLOGIC = new HashSet<>();
 
         int logicInWordCounter = 0;
+        String inputWithNoSpecialCharsWithSpaces = input.replaceAll("[^a-zA-Z0-9]", " ");
 
         int inputLength = input.length();
-        int specialChars = 0;
-        int frazeLength = inputLength - specialChars;
+        System.out.println(input.length());
+
+        String inputWithNoSpecialCharsAndNoSpaces = input.replaceAll("[^a-zA-Z0-9]", "");
+
+        int frazeLength = inputWithNoSpecialCharsAndNoSpaces.length();
+        System.out.println(frazeLength);
 
         List<String> stringList = Arrays.stream(input.split(" ")).collect(Collectors.toList());
 
@@ -25,6 +30,7 @@ public class Task {
 
     public static void main(String[] args) {
 
+        logicOccurrences("I love to work in global logic!");
     }
 
 }
