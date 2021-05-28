3018414da01496f69946eba25f1fe9d85f857ff3
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-28 21:22:38 +0200

    [fix] sorting elements by length

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -3,6 +3,7 @@ package karnecki.blazej;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -15,20 +16,27 @@ public class Task {
 //        charactersSetLOGIC.
 //        Set.of('L', 'O', 'G', 'I', 'C');
 
-        String inputWithNoSpecialCharsWithSpaces = input.toUpperCase().replaceAll("[^a-zA-Z0-9]", " ");
+        String inputWithNoSpecialCharsWithSpaces = input.toUpperCase()
+                .replaceAll("[^a-zA-Z0-9]", " ");
 
         int inputLength = input.length();
 
         String inputWithNoSpecialCharsAndNoSpaces = input.replaceAll("[^a-zA-Z0-9]", "");
         int frazeLength = inputWithNoSpecialCharsAndNoSpaces.length();
+  //      System.out.println(frazeLength);
 
         // Check list of split input
 //      Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList()).forEach(s -> System.out.println(s));
-        List<String> stringList = Arrays.stream(inputWithNoSpecialCharsWithSpaces.split(" ")).collect(Collectors.toList());
+        List<String> stringList = Arrays.stream
+                (inputWithNoSpecialCharsWithSpaces.split(" "))
+                .collect(Collectors.toList());
 
 //        Map<Map<Set<String>, Integer>, Integer> resultDict = new HashMap<>();
 
-        Map<String, Integer> setIntegerMap = new HashMap<>();
+//        Map<String, Integer> setIntegerMap = new HashMap<>();
+        Map<String,Integer> setIntegerMap = new TreeMap<>(
+                Comparator.comparingInt(String::length)
+                        .thenComparing(Function.identity()));
         int totalOccur = 0;
         for (String s : stringList) {
             Set<String> charsInWord = new LinkedHashSet<>();
@@ -48,7 +56,8 @@ public class Task {
             if(!setIntegerMap.containsKey(charsInWord + " " + s.length())) {
                 setIntegerMap.put(charsInWord + " " + s.length(), count);
             } else {
-                setIntegerMap.put(charsInWord + " " + s.length(), setIntegerMap.get(charsInWord + " " + s.length()) + count);
+                setIntegerMap.put(charsInWord + " " + s.length(), setIntegerMap.get(
+                        charsInWord + " " + s.length()) + count);
             }
 //            resultDict.put(setIntegerMap, count);
 //            System.out.println(charsInWord + ", " + setIntegerMap.get(charsInWord) + " wystapienia= " + count + "/" + totalOccur);
@@ -59,11 +68,18 @@ public class Task {
         int finalTotalOccur = totalOccur;
         setIntegerMap.forEach
                 ((k, v) -> System.out.println(
-                        "{" + k  + "} = " + BigDecimal.valueOf(v.doubleValue()/finalTotalOccur).setScale(2, RoundingMode.HALF_UP)
+                        "{" + k
+                                + "} = "
+                                + BigDecimal.valueOf(v.doubleValue()/finalTotalOccur)
+                                .setScale(2, RoundingMode.HALF_UP)
                                 + " " + v + "/" + finalTotalOccur + ")"));
-
-
+        int sumOccur = setIntegerMap.values().stream().mapToInt(Integer::valueOf).sum();
+        System.out.println
+                ("TOTAL Frequency: " +
+                        BigDecimal.valueOf((double) sumOccur/ frazeLength).setScale(2, RoundingMode.HALF_UP) +
+                        " " + sumOccur + "/" + frazeLength + ")");
     }
+
 
     public static void main(String[] args) {
 
