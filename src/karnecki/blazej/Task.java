81e8815395755302794f36baedd29d96d65245d7
[Błażej Karnecki] <blazej.karnecki@gmail.com>
2021-05-29 18:23:54 +0200

    [add] charactersLooked as method parameter

--- src/karnecki/blazej/Task.java
+++ src/karnecki/blazej/Task.java
@@ -5,24 +5,21 @@ import java.math.RoundingMode;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Task {
 
 
-    private static String removeStringSpecialChars(String input){
+    private static String removeStringSpecialChars(String input) {
         return input.toUpperCase()
                 .replaceAll("[^a-zA-Z0-9]", " ");
     }
 
-    private static String trimStringSpecialChars(String input){
+    private static String trimStringSpecialChars(String input) {
         return input.replaceAll("[^a-zA-Z0-9]", "");
 
     }
-    private static void logicOccurrences(String input) {
 
-        Set<Character> charactersSetLOGIC = Stream.of('L', 'O', 'G', 'I', 'C')
-                .collect(Collectors.toCollection(LinkedHashSet::new));
+    private static void logicOccurrences(String input, LinkedHashSet<Character> charactersLookedFor) {
 
         int frazeLength = trimStringSpecialChars(input).length();
 
@@ -30,14 +27,14 @@ public class Task {
                 (removeStringSpecialChars(input).split(" "))
                 .collect(Collectors.toList());
 
-        Map<String,Integer> setIntegerMap = new TreeMap<>(
+        Map<String, Integer> setIntegerMap = new TreeMap<>(
                 Comparator.comparingInt(String::length)
                         .thenComparing(Function.identity()));
         int totalOccur = 0;
         for (String s : stringList) {
             Set<String> charsInWord = new LinkedHashSet<>();
             int count = 0;
-            for (Character c : charactersSetLOGIC) {
+            for (Character c : charactersLookedFor) {
                 if (s.contains(String.valueOf(c))) {
                     charsInWord.add(String.valueOf(c));
                     for (int i = 0; i < s.length(); i++) {
@@ -49,7 +46,7 @@ public class Task {
                 }
             }
 
-            if(!setIntegerMap.containsKey(charsInWord + " " + s.length())) {
+            if (!setIntegerMap.containsKey(charsInWord + " " + s.length())) {
                 setIntegerMap.put(charsInWord + " " + s.length(), count);
             } else {
                 setIntegerMap.put(charsInWord + " " + s.length(), setIntegerMap.get(
@@ -62,20 +59,22 @@ public class Task {
                 ((k, v) -> System.out.println(
                         "{" + k
                                 + "} = "
-                                + BigDecimal.valueOf(v.doubleValue()/finalTotalOccur)
+                                + BigDecimal.valueOf(v.doubleValue() / finalTotalOccur)
                                 .setScale(2, RoundingMode.HALF_UP)
                                 + " " + v + "/" + finalTotalOccur + ")"));
         int sumOccur = setIntegerMap.values().stream().mapToInt(Integer::valueOf).sum();
         System.out.println
                 ("TOTAL Frequency: " +
-                        BigDecimal.valueOf((double) sumOccur/ frazeLength).setScale(2, RoundingMode.HALF_UP) +
+                        BigDecimal.valueOf((double) sumOccur / frazeLength).setScale(2, RoundingMode.HALF_UP) +
                         " " + sumOccur + "/" + frazeLength + ")");
     }
 
 
     public static void main(String[] args) {
+        logicOccurrences("I love to work in global logic!", new LinkedHashSet<>(Arrays.asList('L', 'O', 'G', 'I', 'C')));
+        System.out.println("#################");
+        logicOccurrences("I love to code all day all night!", new LinkedHashSet<>(Arrays.asList('L', 'O', 'V', 'E')));
 
-        logicOccurrences("I love to work in global logic!");
     }
 
 }
